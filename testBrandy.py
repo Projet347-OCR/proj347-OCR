de1962892f2e46ca99f30f33b5b97f022c5af32d
Samson N <samnzumg@gmail.com>
2021-10-23 02:41:10 +0200

    Commit 1 initialize

--- testBrandy.py
+++ testBrandy.py
@@ -1,2 +1,8 @@
 import pytesseract
 
+custom_oem_psm_config = r'--oem 3 --psm 6'
+# pytesseract.image_to_string(image, config=custom_oem_psm_config)
+
+# Example of using pre-defined tesseract config file with options
+cfg_filename = 'words'
+# pytesseract.run_and_get_output(image, extension='txt', config=cfg_filename)
