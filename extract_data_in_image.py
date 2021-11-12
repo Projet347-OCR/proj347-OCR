8005b394865eedad2f02ba5410c1d798276123b5
Samson N <samnzumg@gmail.com>
2021-11-12 20:39:15 +0100

    Commit rebase project

--- extract_data_in_image.py
+++ extract_data_in_image.py
@@ -1,7 +1,14 @@
 import easyocr
+import cv2
 
-ready = easyocr.Reader(['en', 'fr'])
-results = ready.readtext('data/images/permis-de-conduire2.png')
+path_image = 'data/images/new_id_card.png'
+img = cv2.imread(path_image, 0)
+cv2.imshow('Image', img)
+cv2.waitKey(10)
+# cv2.destroyAllWindows()
+
+reader = easyocr.Reader(['en', 'fr'])
+results = reader.readtext(img)
 # print(results)
 
 text = '[ \n    { \n'
@@ -11,8 +18,8 @@ for result in results:
     text += '       "champ '+str(a)+'" : "' + result[1] + '",\n'
 text += '   } \n]'
 
-file = open('data/texte/image_data.txt', 'w')
+file = open('data/output/image_data.txt', 'w')
 file.write(text)
 file.close()
 
-print("Samson")
+print("fin - Samson")
--- image_to_text.py
+++ image_to_text.py
@@ -4,11 +4,11 @@ import json
 ready = easyocr.Reader(['fr'])
 results = ready.readtext('data/images/permis-de-conduire2.png')
 
-# f = open("data/texte/image_data.txt", "r")
+# f = open("data/output/image_data.txt", "r")
 # f_contents = f.read()
 # f.close()
 
-with open('data/texte/data.txt', 'w') as f:
+with open('data/output/data.txt', 'w') as f:
     for result in results:
         print(result[1], file=f)
 
