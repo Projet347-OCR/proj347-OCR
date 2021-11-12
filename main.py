0c9344ae6c943346328e1e941a12486bf1f9cf3a
Brandy MASSADO <63710225+Brandy-MASSADO@users.noreply.github.com>
2021-11-12 17:00:16 +0100

    mAj

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Samson')
+import easyocr
+print('Projet347-OCR (Brandy)')
+
+ready = easyocr.Reader(['en', 'fr'])
+
+results = ready.readtext('data/images/MicrosoftTeams-image.png')
+
+#print(results)
+
+text = ''
+a = 0
+for result in results:
+    a = a + 1
+    text += 'champ '+ ' : ' + result[1] + '\n'
+
+#print(text)
+#print(len(results))
+
+outfile = open('data/texte/test.txt', 'w')
+
+outfile.write(text)
+outfile.close()
