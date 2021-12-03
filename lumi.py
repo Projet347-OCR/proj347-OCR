2182eab1ed883077c4e3b34715348c69030ac04c
zeineb0 <60674654+zeineb0@users.noreply.github.com>
2021-12-03 09:40:54 +0100

    Update lumi.py

--- lumi.py
+++ lumi.py
@@ -7,18 +7,14 @@ cols, rows,npp = img.shape
 brightness = np.sum(img) / (255 * cols * rows)
 minimum_brightness = 0.66
 alpha = brightness / minimum_brightness
-bright_img = cv2.convertScaleAbs(img, alpha = alpha, beta = 255 * (1 - alpha))
-bright_img11 = cv2.convertScaleAbs(img, alpha = 1, beta = 128)
-bright_img1 = cv2.convertScaleAbs(img, alpha = 1, beta = 255 * (minimum_brightness - brightness))
-
+#bright_img = cv2.convertScaleAbs(img, alpha = alpha, beta = 255 * (1 - alpha))
+#bright_img11 = cv2.convertScaleAbs(img, alpha = 2, beta = 128)
+bright_img1 = cv2.convertScaleAbs(img, alpha = 4, beta = 255 * (minimum_brightness - brightness))
 
 cv2.imshow('origin', img)
-
-cv2.imshow('Bilateral', bright_img)
-
+#cv2.imshow('Bilateral', bright_img)
 cv2.imshow('Bilateralurring', bright_img1)
-cv2.imshow('Bilate Blurring', bright_img11)
-
+#cv2.imshow('Bilate Blurring', bright_img11)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
